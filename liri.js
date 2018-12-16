7032d19b0c992bdf28dc3a784aec2fa1ea06cfb4
Mike Worthington <mworthington888@gmail.com>
2018-12-15 21:42:46 -0600

    This push has working Bands-in-Town and Movie-This

--- liri.js
+++ liri.js
@@ -1,19 +1,25 @@
 require("dotenv").config();
 
+const fs = require("fs");
+const axios = require("axios");
+const moment = require('moment');
+
+
+
+var Spotify = require("node-spotify-api");
+//Code required to import the key.js
+var keys = require("./keys.js");
+
+//Access the keys information
 var spotify = new Spotify(keys.spotify);
 
 
-
-// Load the fs package to read and write
-const fs = require("fs");
-const axios = require("axios");
-const spotify = require('spotify');
-
-
 const action = process.argv[2];
-const value = process.argv[3];
+let value = process.argv.slice(3).join(" ");
 
 
+
+//Switch Case for user input of 'Action' ----------------------------------------------------------
 switch (action) {
   case "concert-this":
     concert();
@@ -31,85 +37,89 @@ switch (action) {
     dowhatitsays();
     break;
   }
-  
-   function concert() {
 
-    
-    // let queryUrl = `http://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`;
-    let queryUrl = "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp";
-    console.log(queryUrl);
 
-    console.log(process.argv[2]);
-    console.log(process.argv[3]);
+  // Bands-in-town function ---------------------------------------------------------------------------------
+function concert() {
+       
+    let bandName = value
 
+       let queryUrl = `https://rest.bandsintown.com/artists/${bandName}/events?app_id=codingbootcamp`
+      
     axios.get(queryUrl).then(
-      function(response) {
-        console.log("Release Year: " + response.data.Year);
-        console.log(value);
-      }
-    );
-  // axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp").then(
-  // function(response) {
+    function(response) {
+      console.log(queryUrl);
+    console.log("Venue: " + response.data[0].venue.name);
+    console.log("Location: " + response.data[0].venue.city +", " + response.data[0].venue.country);
+    console.log("Time: " + response.data[0].venue.country);
+    console.log(moment(response.data[0].datetime, "YYYY-MM-DDTHH:mm:ss").utc().format("MM/DD/YYYY"));  
+          });
+  };
+
+
+// Movie function ------------------------------------------------------------------------------------------
+   function movie() {
+      
+    let movieName = value
+
+
+
+    let queryUrl = `http://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`;
+
+    if (!movieName) {//If the user did not type a movie, we display the informations for Mr Nobody
+      let queryUrl = `http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy`;
+      console.log(queryUrl);
+    };
     
-//     console.log("Artist: " + response.data.imdbRating);
-//     console.log("Song Name: " + response.data.imdbRating);
-//     console.log("Album: " + response.data.imdbRating);
-//     console.log("Preview Link: " + response.data.imdbRating);
-
-//   }
-// );
-
-  
-  function spotify() {
-
-  
- 
-  spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-      if ( err ) {
-          console.log('Error occurred: ' + err);
-          return;
-      }
-   
-      // Do something with 'data'
-  });
-  
-
- function movie() {
-
-  const movieName = "";
-
-  if (value = "") {
-    movieName = "Mr. Nobody";
-
-  } else {
-    movieName = value;
-  }
-// Grab the movieName which will always be the third node argument.
-// const movieName = process.argv[2];
-
-// Then run a request with axios to the OMDB API with the movie specified
-const queryUrl = `http://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`;
-
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
-
-axios.get(queryUrl).then(
-  function(response) {
-    console.log("Title: " + response.data.Year);
+    
+    axios.get(queryUrl).then(
+    function(response) {
+    console.log("Title: " + response.data.Title);
     console.log("Release Year: " + response.data.Year);
-    console.log("IMDB rating: " + response.data.Year);
-    console.log("Rottten Tomato rating: " + response.data.Year);
-    console.log("Country: " + response.data.Year);
-    console.log("Language: " + response.data.Year);
-    console.log("Plot: " + response.data.Year);
-    console.log("Actors: " + response.data.Year);
+    console.log("IMDB rating: " + response.data.imdbRating);
+    console.log("Rotten Tomato rating: " + response.data.Ratings[1].Value);
+    console.log("Country: " + response.data.Country);
+    console.log("Language: " + response.data.Language);
+    console.log("Plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+      });
+    };
    
-  }
-);
+    
+  //Spotify function ----------------------------------------------------------------------------------------------
+  // Issues with Spotify-this-song. Continued debugging needed.
+   
 
-  
-
+    // function spotify() {
  
-//  function dowhatitsays() {
-  
+    //   let song = value
 
+    //   spotify.search({ type: 'track', query: song}, function(err, data) {
+    //       if ( err ) {
+    //           console.log('Error occurred: ' + err);
+    //           return;
+    //       }
+      
+        // response.tracks.items.forEach(song=>{
+
+            // console.log("Artist: "+song.artists[0].name);
+            // console.log("Song Name: "+song.name);
+            // console.log("Preview Link: "+song.preview_url);
+             // console.log("The Song Album: "+song.album.name);
+           
+    //     });
+
+    //   });
+    // }
+
+    //Do-What-It-Says function still needs debugging. Issues with data within random.txt file across
+    // all three functions (Concert-this, Spotify-this and Movie-this)
+
+    function dowhatitsays(){
+      fs.readFile('random.txt', "utf8", function(error, data){
+        var randomText = data.split(',');
+    
+        concert(randomText[1]);
+      });
+    }
+    
